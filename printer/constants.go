e4e5e58fadf9b14dbe9cba1d7ad5679f9754d4e6
João Ribeiro <joao.ribeiro@foursource.pt>
2020-10-28 17:05:36 +0000

    first commit

--- printer/constants.go
+++ printer/constants.go
@@ -3,13 +3,18 @@ package printer
 const (
 	// Lowest quality
 	QualityLow Quality = iota
+
 	// Normal quality
 	QualityNormal
+
 	// High quality
 	QualityHigh
 )
 
 const (
+	// Default brand name
 	DefaultBrand = "New"
+
+	// Default model code
 	DefaultModel = 404
 )
